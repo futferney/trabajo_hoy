27fc321d69ecaa93f03530bad77c61f5587b3a79
Estefania <estefaniajimenezpana@gmail.com>
2023-08-18 22:21:11 -0500

    cambios de servios

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ const router = Router()
 
 
 router.get('/',(req, res)=> res.render('index', {tittle:'Bienvenido al index'}))
-router.get('/about',(req, res)=> res.render('about', {tittle:'este es el about'}))
+router.get('/servicios',(req, res)=> res.render('servicios', {tittle:'este es el about'}))
 router.get('/contact',(req, res)=> res.render('contact', {tittle:'este es el contact'}))
+router.get('/quienes',(req, res)=> res.render('quienes', {tittle:'este es el contact'}))
 
 export default router
