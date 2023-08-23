a7bd5aeab47ae8d23a984702083768ac6060c324
jvega9 <yeisonjvega98@gmail.com>
2023-08-23 18:59:16 -0500

    cambios de paginas y agregacion de nuevo contenido

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,8 @@ router.get('/',(req, res)=> res.render('index', {tittle:'Bienvenido al index'}))
 router.get('/servicios',(req, res)=> res.render('servicios', {tittle:'esto es servicios'}))
 router.get('/contact',(req, res)=> res.render('contact', {tittle:'este es el contact'}))
 router.get('/quienes',(req, res)=> res.render('quienes', {tittle:'este es el contact'}))
+router.get('/productos',(req, res)=> res.render('productos', {tittle:'sona de productos'}))
+router.get('/formulario',(req, res)=> res.render('formulario', {tittle:'formularios'}))
+router.get('',(req, res)=> res.render('', {tittle:'este es el contact'}))
 
 export default router
